c7ce3e4bbdb17ce962ae831b9ef3e1e42f07855c
Nam_ubuntu <doanngocnam3002@gmail.com>
2025-02-28 14:02:32 +0700

    section 2

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression')
+
+// Middlewares
+app.use(morgan('dev')); // HTTP request logger middleware
+app.use(helmet()); // Secure Express apps by setting various HTTP headers
+app.use(compression()); // Node.js compression middleware
+
+
+app.get('/', (req, res) => {
+    const strCompress='Hello World';
+    return res.status(200).json({
+        message: 'Hello World',
+        metadata: strCompress.repeat(10000)
+    });
+}
+);
+
+module.exports = app;
