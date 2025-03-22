568c371a3e8fc6986d04df51f1ba43f222ddd3f4
Nam_ubuntu <doanngocnam3002@gmail.com>
2025-03-22 14:38:07 +0700

    Update(model): nested attributes/hide post.hhtp

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const server = app.listen(8080,()=>{
     console.log('Server is running on port 8080');
 })
 
+// 
 process.on('SIGINT',()=>{
     server.close(()=>{
         console.log('Server is closed');
     });
-
 })
